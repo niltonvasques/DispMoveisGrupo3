a90711d15c29621340a1e791d45afbefec5a48f2
Nilton Vasques <nilton.vasques@gmail.com>
2014-12-11 19:43:31 -0300

    Configurado os Push notifications

--- src/BotaoDoPanico/src/br/ufba/matc89/botaodopanico/BotaoDoPanicoApplication.java
+++ src/BotaoDoPanico/src/br/ufba/matc89/botaodopanico/BotaoDoPanicoApplication.java
@@ -1,20 +1,34 @@
 package br.ufba.matc89.botaodopanico;
 
 import android.app.Application;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
-import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 public class BotaoDoPanicoApplication extends Application{
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		Parse.initialize(this, "pU5pZWL3KWOHTWWk2E0V1Ej54gvABbWHDdk31SQy", "i3FvaOGnXq3IvWJSwkxpdaBT7FyWGL3cvRfK0ih8");
 		ParseFacebookUtils.initialize("348131288692341");
 		
+		System.out.println("Subscribing...");
+
+		ParsePush.subscribeInBackground("", new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e == null) {
+					Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+				} else {
+					Log.e("com.parse.push", "failed to subscribe for push", e);
+				}
+			}
+		});
 	}
 
 }
